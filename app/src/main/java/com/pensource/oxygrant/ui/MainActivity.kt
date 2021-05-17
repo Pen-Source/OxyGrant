d60e4de90f0087f3c90038c20931a464dc4af64e
haseebcgt <abdul.haseeb@cgtindia.in>
2021-05-17 22:54:44 +0530

    Moved MainActivity.kt to ui package

--- app/src/main/java/com/pensource/oxygrant/ui/MainActivity.kt
+++ app/src/main/java/com/pensource/oxygrant/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.pensource.oxygrant
+package com.pensource.oxygrant.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pensource.oxygrant.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
