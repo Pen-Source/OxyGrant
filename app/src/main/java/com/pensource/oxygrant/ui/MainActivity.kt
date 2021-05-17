aa3596aa121eed04acb6e5afaef222866cd27742
haseebcgt <abdul.haseeb@cgtindia.in>
2021-05-18 01:25:42 +0530

    Added home fragment

--- app/src/main/java/com/pensource/oxygrant/ui/MainActivity.kt
+++ app/src/main/java/com/pensource/oxygrant/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.pensource.oxygrant.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pensource.oxygrant.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
