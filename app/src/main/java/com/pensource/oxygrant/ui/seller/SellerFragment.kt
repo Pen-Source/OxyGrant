e824e5668527a46c84e97877a6b51733f9f52bf9
haseebcgt <haseebpvt@gmail.com>
2021-05-20 23:58:54 +0530

    Seller fragment ViewModel and design

--- app/src/main/java/com/pensource/oxygrant/ui/seller/SellerFragment.kt
+++ app/src/main/java/com/pensource/oxygrant/ui/seller/SellerFragment.kt
@@ -5,9 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.pensource.oxygrant.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SellerFragment : Fragment() {
+
+    private val sellerViewModel: SellerViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
