fcb7052bf860c8ac9439d531bbc2f35e10a0ab32
haseebcgt <haseebpvt@gmail.com>
2021-05-26 23:16:09 +0530

    Added data source to di

--- shared/src/main/java/com/pensource/shared/di/AppModule.kt
+++ shared/src/main/java/com/pensource/shared/di/AppModule.kt
@@ -1,10 +1,15 @@
 package com.pensource.shared.di
 
 import com.google.firebase.auth.FirebaseAuth
+import com.pensource.shared.data.supply.FirebaseSupplyDataSource
+import com.pensource.shared.data.supply.FirebaseSupplyRepository
+import com.pensource.shared.data.supply.SupplyDataSource
+import com.pensource.shared.data.supply.SupplyRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
 @Module
@@ -13,5 +18,17 @@ object AppModule {
     @Provides
     fun provideFirebaseAuth(): FirebaseAuth {
         return FirebaseAuth.getInstance()
+    }
+
+    @Singleton
+    @Provides
+    fun provideSupplyDataSource(): SupplyDataSource {
+        return FirebaseSupplyDataSource()
+    }
+
+    @Singleton
+    @Provides
+    fun provideSupplyRepository(): SupplyRepository {
+        return FirebaseSupplyRepository()
     }
 }
