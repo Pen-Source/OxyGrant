46076059e06a060efe87756d00a2118f58046fd4
haseebcgt <haseebpvt@gmail.com>
2021-06-01 18:33:59 +0530

    Loading current user supply

--- app/src/main/java/com/pensource/oxygrant/ui/seller/SellerViewModel.kt
+++ app/src/main/java/com/pensource/oxygrant/ui/seller/SellerViewModel.kt
@@ -3,24 +3,35 @@ package com.pensource.oxygrant.ui.seller
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.NavDirections
 import com.pensource.model.Supply
+import com.pensource.shared.domain.auth.GetFirebaseUserUseCase
+import com.pensource.shared.domain.supply.GetCurrentUserSupplyUseCase
 import com.pensource.shared.result.Event
+import com.pensource.shared.result.Result
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SellerViewModel @Inject constructor() : ViewModel() {
+class SellerViewModel @Inject constructor(
+    private val getCurrentUserSupplyUseCase: GetCurrentUserSupplyUseCase,
+    private val getFirebaseUserUseCase: GetFirebaseUserUseCase
+) : ViewModel() {
 
     private val _actionAddSupply = MutableLiveData<Event<NavDirections>>()
     val actionAddSupply: LiveData<Event<NavDirections>> = _actionAddSupply
 
     private val _supplyList = MutableLiveData<List<Supply>>().apply {
-        // Load fake supply data for testing list view
-        value = listOf(
-            Supply("abc", "abc", "Supplier 1", "1234567890", false, quantity = 50.0, 0f, 0f),
-            Supply("abc", "abc", "Supplier 1", "1234567890", true, quantity = 50.0, 0f, 0f, "", true)
-        )
+        viewModelScope.launch {
+            val userId = getFirebaseUserUseCase()?.uid ?: return@launch
+            when (val result = getCurrentUserSupplyUseCase(userId)) {
+                is Result.Success -> {
+                    value = result.data
+                }
+            }
+        }
     }
     val supplyList: LiveData<List<Supply>> = _supplyList
 
